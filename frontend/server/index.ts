688e019c0979759599562efd6a151810bef05164
OPRahul56267 <rp5296045@gmail.com>
2025-09-16 16:25:17 +0530

    third remove server

--- frontend/server/index.ts
+++ frontend/server/index.ts
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
   }
 
   // Choose host based on platform
-  const port = parseInt(process.env.PORT || "5000", 10);
+  const port = parseInt(process.env.PORT || "3000", 10);
   const host = process.platform === "win32" ? "127.0.0.1" : "0.0.0.0";
 
   server.listen(port, host, () => {
